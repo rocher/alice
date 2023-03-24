f1dfa56a387463b5bc3419fa5f5d4fc4eb37687d
Francesc Rocher <francesc.rocher@gmail.com>
2023-03-24 10:11:27 +0100

    New Problem and CLI template files

---
+++ project_euler/src/cli/c0000_template.adb
@@ -0,0 +1,18 @@
+-------------------------------------------------------------------------------
+--
+--  ALICE - Adventures for Learning and Inspiring Coding Excellence
+--  Copyright (c) {YEAR} {NAME} {EMAIL}
+--  SPDX-License-Identifier: MIT
+--
+-------------------------------------------------------------------------------
+
+with P{0NUM}_{PACKAGE_TITLE};
+with Project_Euler.CLI.Runner;
+
+procedure C{0NUM}_{PACKAGE_TITLE} is
+   Problem : P{0NUM}_{PACKAGE_TITLE}.Problem_Type;
+begin
+
+   Project_Euler.CLI.Runner.Run (Problem);
+
+end C{0NUM}_{PACKAGE_TITLE};
---
+++ project_euler/src/problem/p0000_template.adb
@@ -0,0 +1,33 @@
+-------------------------------------------------------------------------------
+--
+--  ALICE - Adventures for Learning and Inspiring Coding Excellence
+--  Copyright (c) {YEAR} {NAME} {EMAIL}
+--  SPDX-License-Identifier: MIT
+--
+--  ---------------------------------------------------------------------------
+--
+--  The following problem is taken from Project Euler:
+--
+--                 https://projecteuler.net/problem={NUMBER}
+--
+--  {TITLE}
+--  -------
+--
+--  {DESCRIPTION}
+--
+--  {BRIEF}
+--
+-------------------------------------------------------------------------------
+
+with Euler_Tools; use Euler_Tools;
+
+package body P{0NUM}_{PACKAGE_TITLE} is
+
+   overriding function Answer (Problem : Problem_Type) return String is
+      Answer : Natural;
+   begin
+
+      return To_String (Answer);
+   end Answer;
+
+end P{0NUM}_{PACKAGE_TITLE};
