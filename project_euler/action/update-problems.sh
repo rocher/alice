b6a60c214fba6de26e3d93ff862c1fac832da752
Francesc Rocher <francesc.rocher@gmail.com>
2023-04-09 11:18:02 +0200

    Update participant repos

--- project_euler/action/update-problems.sh
+++ project_euler/action/update-problems.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+set -o errexit
+
+CWD=$(dirname $(realpath $0))
+
+# Update problems
 if [ -d problems ]; then
     cd problems
-    echo "-- Updating problems directory"
+    echo -e "\n-- Updating problems directory"
     git pull --all --tags --force
 else
-    echo "-- Cloning problems directory"
+    echo -e "\n-- Cloning problems directory"
     git clone git@github.com:rocher/Alice_Project_Euler_Problems problems
 fi
+
+# Update participant repositories
+USR=$(realpath ${CWD}/../usr/)
+for repo in $(find ${USR} -mindepth 1 -maxdepth 1 -type d); do
+    cd $repo >/dev/null 2>&1
+    echo -e "\n-- Updating repository usr/$(basename $repo)"
+    git pull --all --tags --force
+    cd - >/dev/null 2>&1
+done
